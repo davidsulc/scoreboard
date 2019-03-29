7e62b02baf16eb3e4aceb1c8d64c081affbaacff
David Sulc <davidsulc@gmail.com>
2019-03-29 19:18:31 +0100

    basic counter implementation

--- lib/scoreboard_web/router.ex
+++ lib/scoreboard_web/router.ex
@@ -18,7 +18,7 @@ defmodule ScoreboardWeb.Router do
   scope "/", ScoreboardWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    live("/", ScorekeeperLive)
   end
 
   # Other scopes may use custom stacks.
