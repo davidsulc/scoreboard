eda974ebe708c365082672bbd0dbb57e40f1c40a
David Sulc <davidsulc@gmail.com>
2019-03-30 08:53:29 +0100

    generated scoreboard channel

--- lib/scoreboard_web/channels/user_socket.ex
+++ lib/scoreboard_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ScoreboardWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", ScoreboardWeb.RoomChannel
+  channel "scoreboard:*", ScoreboardWeb.ScoreboardChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
