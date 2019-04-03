d7d19d1544858e0216769141ade11c97a17c1696
David Sulc <davidsulc@gmail.com>
2019-04-03 09:43:19 +0200

    removed debugging log call

--- lib/scoreboard_web/live/scorekeeper_live.ex
+++ lib/scoreboard_web/live/scorekeeper_live.ex
@@ -3,14 +3,11 @@ defmodule ScoreboardWeb.ScorekeeperLive do
 
   alias ScoreboardWeb.ScorekeeperView
 
-  require Logger
-
   def render(assigns) do
     ScorekeeperView.render("index.html", assigns)
   end
 
   def mount(_session, socket) do
-    Logger.error(connected?(socket), label: "socket connected?")
     {:ok, %{current_set: {a, b}} = state} = Scoreboard.GameState.state()
 
     socket =
